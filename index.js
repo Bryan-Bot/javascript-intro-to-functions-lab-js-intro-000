e438bcfb26b0c5c5dbb847c138f22be7b6e3d3c0
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-06-02 18:43:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
-    return "I cant hear you"
+    return "I cant hear you!"
   }
-  else if (string === string.toUpperCase) {
-    return "YES INDEED"
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
     return "I love you, too."
